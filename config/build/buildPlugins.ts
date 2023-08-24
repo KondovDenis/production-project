d024f5e1d6cb309f12401d7313eafb9db2a802fb
Денис Кондов <deniskondov@MacBook-Air-Denis.local>
2023-08-24 11:57:12 +0300

    Add Git Actions TESTING + BUILDING fix08

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -28,7 +28,7 @@ export function buildPlugins({paths, isDev}:BuildOptions): webpack.WebpackPlugin
     ]
 
     if (isDev){
-        plugins.push(new ReactRefreshWebpackPlugin()),
+        plugins.push(new ReactRefreshWebpackPlugin())
         plugins.push(new BundleAnalyzerPlugin({openAnalyzer:false}))
     }
     return plugins 
