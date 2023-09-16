68482a7d465ca27c79a2366f52ce8476ce986217
Денис Кондов <deniskondov@MacBook-Air-Denis.local>
2023-09-17 00:54:42 +0300

    Update sidebar

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,8 +3,20 @@ import cls from './Sidebar.module.scss';
 import { FC, useState } from 'react';
 import { ThemeSwitcher } from '../../../../shared/ui/ThemeSwitcher';
 import { LangSwitcher } from '../../../LangSwitcher';
-import { Button } from '../../../../shared/ui/Button/Button';
+import { Button, ButtonSize } from '../../../../shared/ui/Button/Button';
 import React from 'react';
+import { Theme } from "../../../../app/providers/ThemeProvider/lib/ThemeContext";
+import MenuLight from "../Sidebar/assets/icons/menu_light.svg"
+import MenuDark from "../Sidebar/assets/icons/menu_dark.svg"
+import { useTheme } from '../../../../app/providers/ThemeProvider';
+import { ThemeButton } from '../../../../shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
+import { AppLink, AppLinkTheme } from "../../../../shared/ui/AppLink/AppLink";
+import {RoutePath} from '../../../../shared/config/routeConfig/routeConfig'
+import MainIcon from '../../../../shared/assets/icons/main-20-20.svg'
+
+
+
 
 interface SidebarProps  {
     className?: string;
@@ -15,7 +27,8 @@ interface SidebarProps  {
 
 export const Sidebar: FC<SidebarProps> = ({className}:SidebarProps) => {
 
-
+    const {t} = useTranslation('navbar')
+    const {theme, toggleTheme} = useTheme()
     const [collapsed, setCollapsed] = useState(false)
 
     const onToggle = () => {
@@ -24,8 +37,16 @@ export const Sidebar: FC<SidebarProps> = ({className}:SidebarProps) => {
 
     return(
 	<div data-testid='sidebar' className={classNames(cls.Sidebar,{ [cls.collapsed]:collapsed },[className])}>
-        <Button data-testid='sidebar-toggle' onClick={onToggle}>toggle</Button>
-        <div className={cls.switchers}>
+        <Button theme= {ThemeButton.CLEAR} data-testid='sidebar-toggle' className = {cls.collapseBtn} onClick={onToggle} size={ButtonSize.XL}>{theme == Theme.DARK?<MenuLight/>:<MenuDark/>}</Button>
+        <div className={cls.items}>
+            <div>
+	    	<AppLink theme = {AppLinkTheme.PRIMARY} to={RoutePath.main}  className={cls.item}>
+			<MainIcon className={cls.icon}></MainIcon>
+			<span className={cls.link}>{t('Главная')}</span>
+		</AppLink>
+	    </div>
+	</div>
+	<div className={cls.switchers}>
             <ThemeSwitcher></ThemeSwitcher>
             <LangSwitcher className={cls.lang}></LangSwitcher>
         </div>
