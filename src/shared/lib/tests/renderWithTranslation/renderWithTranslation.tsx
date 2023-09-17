472f9cd5b790c450113c430150fd46dc8b8e2b57
Денис Кондов <deniskondov@MacBook-Air-Denis.local>
2023-09-17 18:41:21 +0300

    Add route in test

--- src/shared/lib/tests/renderWithTranslation/renderWithTranslation.tsx
+++ src/shared/lib/tests/renderWithTranslation/renderWithTranslation.tsx
@@ -3,13 +3,25 @@ import { I18nextProvider } from "react-i18next";
 import { render } from "@testing-library/react";
 import React from "react";
 import i18nForTests from '../../../config/i18n/i18nForTest'
-import {BrowserRouter} from 'react-router-dom';
+import {MemoryRouter} from 'react-router-dom';
 
 
-export function renderWithTranslation(component:ReactNode){
+
+export interface componentRenderOptions{
+
+	route?: string;
+}
+
+
+
+
+export function renderWithTranslation(component:ReactNode, options:componentRenderOptions = {}){
+
+    const {route='/',} = options
+    
     return render (
-    <BrowserRouter>
+    <MemoryRouter initialEntries={[route]}>
         <I18nextProvider i18n={i18nForTests}>{component}</I18nextProvider>
-    </BrowserRouter>
+    </MemoryRouter>
     )
 }
