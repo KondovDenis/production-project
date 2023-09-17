132f2d11cd4f93d1d6834c6baff608ad00d61f16
Денис Кондов <deniskondov@MacBook-Air-Denis.local>
2023-09-17 17:27:59 +0300

    Update test

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,6 +1,6 @@
 import {Sidebar} from './Sidebar'
 import {fireEvent, render, screen} from '@testing-library/react'
-import React from 'react'
+import React, {Component} from 'react'
 import { withTranslation } from 'react-i18next'
 import { renderWithTranslation } from '../../../../shared/lib/tests/renderWithTranslation/renderWithTranslation'
 
